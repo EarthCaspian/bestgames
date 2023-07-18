d26822c324ebfee4374c2d9f1485b7ee84e2a280
EarthCaspian <hazar.a@outlook.com>
2023-07-18 18:27:56 +0300

    registered comment for admin and returned str

--- oyunlar/models.py
+++ oyunlar/models.py
@@ -41,5 +41,8 @@ class Comment(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.content
+
     class Meta:
         ordering = ['-created_at']
