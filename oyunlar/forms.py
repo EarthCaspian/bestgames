3ffd2eaaa7a05a91a1b4ad5f63215df0b2c6ec46
EarthCaspian <hazar.a@outlook.com>
2023-07-18 14:52:44 +0300

    added labels to game objects to display on the dom

--- oyunlar/forms.py
+++ oyunlar/forms.py
@@ -5,6 +5,14 @@ class GameForm(ModelForm):
     class Meta:
         model = Game
         fields = ['oyunTuru', 'oyunPlatformu', 'oyunCikisTarihi', 'oyunIsim', 'oyunAciklama', 'oyunResim']
+        labels = {
+            'oyunTuru': 'Game Genre',
+            'oyunPlatformu': 'Game Platform',
+            'oyunCikisTarihi': 'Release Date',
+            'oyunIsim': 'Game Name',
+            'oyunAciklama': 'Game Description',
+            'oyunResim': 'Game Image',
+        }
     
     def __init__(self, *args,**kwargs):
         super(GameForm, self).__init__(*args, **kwargs)
