78916c0cc4000f30ce7238aa9c36354046b076a6
EarthCaspian <hazar.a@outlook.com>
2023-07-21 21:31:07 +0300

    date can now be custom added and minor display fix

--- oyunlar/forms.py
+++ oyunlar/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import *
+from django.forms import widgets
 
 class GameForm(ModelForm):
     class Meta:
@@ -18,3 +19,5 @@ class GameForm(ModelForm):
         super(GameForm, self).__init__(*args, **kwargs)
         for name, field in self.fields.items():
             field.widget.attrs.update({'class':'form-control'})
+        # Change the widget for the release date field to DateInput
+        self.fields['oyunCikisTarihi'].widget = widgets.DateInput(attrs={'type': 'date'})
