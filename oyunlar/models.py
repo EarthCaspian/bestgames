eebf8403d5ab39d0e4868974f92c11d851155dff
EarthCaspian <hazar.a@outlook.com>
2023-07-24 15:23:54 +0300

    created upvote model

--- oyunlar/models.py
+++ oyunlar/models.py
@@ -46,3 +46,11 @@ class Comment(models.Model):
 
     class Meta:
         ordering = ['-created_at']
+
+class Upvote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'game')
