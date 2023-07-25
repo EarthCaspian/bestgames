67bd19b1d6d15dfa9eca9f191e57bcbc9d779e1c
EarthCaspian <hazar.a@outlook.com>
2023-07-25 17:25:24 +0300

    upvote function progress

--- static/main.js
+++ static/main.js
@@ -94,6 +94,23 @@ document.querySelector('form').addEventListener('submit', function(event) {
 
 //* Upvote function
 
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === `${name}=`) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
 document.addEventListener('DOMContentLoaded', () => {
   const upvoteButtons = document.querySelectorAll('.upvote-button');
 
